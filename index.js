caf0f1aec9addff32fde116ff0c65639f6c2b18c
Ahmed1khaled1 <ahmedkhaledg49@gmail.com>
2024-12-23 22:24:44 +0200

    Initial commi1t

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
+import pkg from "cloudinary";
+const { v2: cloudinary } = pkg;
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
@@ -19,6 +23,8 @@ import verifyToken from "./middleware/auth.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
+
+
 const app = express();
 app.use(express.json());
 app.use(helmet());
@@ -29,15 +35,22 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({ origin: "*" }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
+/* CLOUDINARY CONFIGURATION */
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 /* FILE STORAGE */
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/assets");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "Social App",
+    public_id: (req, file) => Date.now() + "-" + file.originalname,
   },
 });
+
 const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
