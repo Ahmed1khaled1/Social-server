8be56cdabe05061078e48ce91055b1922b3ec544
Ahmed-khaled <158309985+Ahmed1khaled1@users.noreply.github.com>
2024-12-27 11:19:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,8 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({origin: "*"}));
+app.use(cors({ origin: "*" }));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
 
 /* CLOUDINARY CONFIGURATION */
 cloudinary.config({
